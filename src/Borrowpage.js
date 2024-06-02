77907802809f23f6d68a44e09a71cd6d43c9bc08
THEmustigaminghd <theofficialmusti@gmail.com>
2024-06-02 11:22:00 +0100

    added loaded bar and deposit page mimiked from borrow page selected

--- src/Borrowpage.js
+++ src/Borrowpage.js
@@ -5,12 +5,13 @@ const BorrowPage = ({ handleClick }) => {
   const navigate = useNavigate();
   return (
     <header className="App-header">
-      <h2>Pick your vault you want to lend to</h2>
+      <h2>Pick your vault you want to borrow from</h2>
+
       {/* Add your lending content here */}
         <div>
-          <button  className="button-lend" onClick={() => navigate('/borrowselected')}>Select</button>
-          <button  className="button-lend" onClick={() => navigate('/borrowselected')}>Select</button>
-          <button  className="button-lend" onClick={() => navigate('/borrowselected')}>Select</button>
+          <button  className="button-lend" onClick={() => navigate('/borrowselected')}>6 month vault</button>
+          <button  className="button-lend" onClick={() => navigate('/borrowselected')}>12 month vault</button>
+          <button  className="button-lend" onClick={() => navigate('/borrowselected')}>24 month vault</button>
         </div>
   </header>
   );
